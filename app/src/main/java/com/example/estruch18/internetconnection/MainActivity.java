b2e37ef63bdaee814c6e6073c81cbb8b8a03dd49
estruch18 <estruch95.b@gmail.com>
2016-01-13 19:21:15 +0100

    Finalizada

--- app/src/main/java/com/example/estruch18/internetconnection/MainActivity.java
+++ app/src/main/java/com/example/estruch18/internetconnection/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.estruch18.internetconnection;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -15,15 +17,22 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imagen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imagen = (ImageView) findViewById(R.id.imagenCargada);
+
         //Ejecución de métodos
-        internetConnection();
+        Log.d("ivan", "Abans del asynctask");
+        new BaixaImatge().execute();
+        Log.d("ivan", "Despres del asynctask");
     }
 
     @Override
@@ -48,46 +57,59 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void internetConnection(){
-        InputStream is = null;
 
-        try {
-            URL url = new URL("http://www.upcomingmobilephone.com/wp-content/uploads/2015/11/best-10-mobile-phone.jpg");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(10000);
-            conn.setConnectTimeout(15000);
-            conn.setRequestMethod("GET");
-            conn.setDoInput(true); /*vamos a recibir datos*/
+    public class BaixaImatge extends AsyncTask<Void, Void, Bitmap> {
 
-            //Empieza la conexión
-            conn.connect();
-            int response = conn.getResponseCode();
-            is = conn.getInputStream();
+        @Override
+        protected Bitmap doInBackground(Void... params) {
+            InputStream is = null;
+            Bitmap bitmap = null;
 
-            //Convertimos el InputStream con los datos recibidos a un BitMap
-            Bitmap bitmap = BitmapFactory.decodeStream(is);
-            ImageView imageView = (ImageView)findViewById(R.id.imagenCargada);
-            imageView.setImageBitmap(bitmap);
+            try {
+                Log.d("ivan", "connectant...");
+                URL url = new URL("http://khammambusinessdirectory.com/wp-content/uploads/2014/11/147-50x50.jpg");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true); /*vamos a recibir datos*/
 
-        }
-        catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        catch (ProtocolException e) {
-            e.printStackTrace();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        finally {
-            if(is != null){
-                try{
-                    is.close();
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
+                //Empieza la conexión
+                conn.connect();
+                int response = conn.getResponseCode();
+                is = conn.getInputStream();
+
+                //Convertimos el InputStream con los datos recibidos a un BitMap
+                 bitmap = BitmapFactory.decodeStream(is);
+                Log.d("ivan", "Ja he rebut la imatge");
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (is != null) {
+                    try {
+                        is.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
+            return bitmap;
         }
-    }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            //super.onPostExecute(bitmap);
+            Log.d("ivan", "inici onPost");
+            imagen.setImageBitmap(bitmap);
+            Log.d("ivan", "Final on post");
+        }
+    }  // del AsyncTask
+
+
+
 }
